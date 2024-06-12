4a8ddf1bdc2d7f0da8bee3ace47adb6229a41722
terezo01 <thegames0079@gmail.com>
2024-06-12 11:25:13 -0300

    Update

--- Fibonacci/script.js
+++ Fibonacci/script.js
@@ -2,11 +2,12 @@ function calcularFor(){
     num = parseInt(document.getElementById('num').value);
     a = 0;
     b = 1;
-    for(i=0; i<=num; i++){
+    fib = ""
+    for(i=0; i<=num - 3; i++){
         res = a + b
         a = b
         b = res
-        fib = fib + res + ","
+        fib = fib + res  + ", "
     }
-    document.getElementById("saida").innerHTML = fib
+    document.getElementById("saida").innerHTML = `0, 1, ${fib}`
 }
