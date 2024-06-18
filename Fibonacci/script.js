610a1bd589c04a8c855fc682e07d98697e86c31b
terezo01 <thegames0079@gmail.com>
2024-06-18 10:03:13 -0300

    10

--- Fibonacci/script.js
+++ Fibonacci/script.js
@@ -1,35 +1,52 @@
 function calcularFor(){
     num = parseInt(document.getElementById('num').value);
+    num1 = document.getElementById("num").value;
     a = 0;
     b = 1;
     fib = "";
-    for(i=0; i<=num - 3; i++){
+
+    for(i=0; i<num - 2; i++){
         res = a + b;
         a = b;
         b = res;
         fib = fib + res  + ", ";
     }
-    document.getElementById("saida").innerHTML = `0, 1, ${fib}`;
+
+    if(num < 2 || num1 == ""){
+        alert("Escreva um número maior que 1")    
+    }
+    else{
+        document.getElementById("saida").innerHTML = `0, 1, ${fib}`;
+    }
 }
 
 function calcularWhile(){
     num = parseInt(document.getElementById('num').value);
+    num1 = document.getElementById("num").value;
     a = 0;
     b = 1;
     fib = "";
     i=0;
-    while(i<=num - 3){
+
+    while(i<num - 2){
         res = a + b;
         a = b;
         b = res;
         fib = fib + res  + ", ";
         i++;
     }
-    document.getElementById("saida").innerHTML = `0, 1, ${fib}`;
+
+    if(num < 2 || num1 == ""){
+        alert("Escreva um número valido")    
+    }
+    else{
+        document.getElementById("saida").innerHTML = `0, 1, ${fib}`;
+    }
 }
 
 function calcularDo(){
     num = parseInt(document.getElementById('num').value);
+    num1 = document.getElementById("num").value;
     a = 0;
     b = 1;
     fib = "";
@@ -40,6 +57,12 @@ function calcularDo(){
         b = res;
         fib = fib + res  + ", ";
         i++;
-    }while(i<=num - 3)
-    document.getElementById("saida").innerHTML = `0, 1, ${fib}`;
+    }while(i<num - 2)
+
+    if(num < 2 || num1 == ""){
+        alert("Escreva um número valido")    
+    }
+    else{
+        document.getElementById("saida").innerHTML = `0, 1, ${fib}`;
+    }
 }
